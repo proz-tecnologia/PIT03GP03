eb814ebd52871e6d52d1dc875ccf69ab5f7843db
marlossantana <marlossantanaa@gmail.com>
2023-01-24 16:38:19 -0300

    Merge remote-tracking branch 'origin/MarlosTeste4'

--- lib/view/pages/login/signup_pages/page_name.dart
+++ lib/view/pages/login/signup_pages/page_name.dart
@@ -18,72 +18,76 @@ class _PageNameState extends State<PageName> {
   @override
   Widget build(BuildContext context) {
 
-    return  Container(
-      key: fomrKey,
-      color: Colors.white54,
+    return  Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        key: fomrKey,
+        color: Colors.white54,
 
-      child: Column(
-
-
-
-        children: [
-          SizedBox(
-            height: 100,
-          ),
-
-        Text(
-        "Boas-Vindas ao GREEN !",
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
-            color: AppColors.black),
-
-      ),
-          SizedBox(
-            height: 5,
-          ),
-      Text('Para começar ,qual o seu NOME?.',style: TextStyle(
-        fontSize: 20,fontWeight: FontWeight.bold
-      ),),
+        child: Column(
 
 
-      SizedBox(height: 10,),
-          Text('Precisamos dele para dar inicio ao seu cadastro.',style: TextStyle(
-              fontSize: 16,color: AppColors.grey
-          ),),Align(
-            alignment: Alignment.bottomLeft,
-          ),
+
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+
+          Text(
+          "Boas-Vindas ao GREEN !",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: AppColors.black),
+
+        ),
+            SizedBox(
+              height: 5,
+            ),
+        Text('Para começar ,qual o seu NOME?.',style: TextStyle(
+          fontSize: 20,fontWeight: FontWeight.bold
+        ),),
+
+
+        SizedBox(height: 10,),
+            Text('Precisamos dele para dar inicio ao seu cadastro.',style: TextStyle(
+                fontSize: 16,color: AppColors.grey
+            ),),Align(
+              alignment: Alignment.bottomLeft,
+            ),
 SizedBox(
   height: 20,
 ),
-      TextFormField(
-       controller: name,
-        cursorColor: AppColors.black,
+        TextFormField(
+         controller: name,
+          cursorColor: AppColors.black,
+autofocus: true,
+          autocorrect: true,
+          autovalidateMode:
+          AutovalidateMode.onUserInteraction,
+          validator: (String? value) {
+            if (value!.isEmpty) {
+              return "required_field".i18n();
+            }
 
-        autocorrect: true,
-        autovalidateMode:
-        AutovalidateMode.onUserInteraction,
-        validator: (String? value) {
-          if (value!.isEmpty) {
-            return "required_field".i18n();
-          }
+            return null;
+          },
+          style: TextStyle(
+              fontSize: 17,
 
-          return null;
-        },
-        style: TextStyle(
-            fontSize: 17,
+              color:Colors.black),
+          decoration: InputDecoration(
+              prefixIcon: Icon(Icons.alternate_email, color: Colors.black.withOpacity(.3),),
 
-            color:Colors.black),
-        decoration: InputDecoration(
+            hintStyle: TextStyle(
 
-            hintText: " Maria do Socorro",
-          hintStyle: TextStyle(
-            color: Colors.grey
-          )
+              color: Colors.grey
+            )
+          ),
         ),
-      ),
-    ]),
+      ]),
 
+      ),
     );
 
 
